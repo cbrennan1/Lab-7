3720c7c02ebbbf56840e4dd649a4f47ee65985fe
cbrennan1 <44119782+cbrennan1@users.noreply.github.com>
2019-11-21 15:56:11 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-function init() {
+function init(){
+
+var text = document.getElementById('entryinput');
+var results = document.getElementById('textoutput');
+var button = document.getElementById('entrybutton');
+
+button.addEventListener('click', function(){
+  alert('Colin Brennan: ' + text.value);
+});
+
+button.addEventListener('click', function(){
+  results.innerHTML = text.value;
+});
 
 
-	var message = document.getElementById("entryinput");
-	var button = document.getElementById("entrybutton");
-	var showresults = document.getElementById('textoutput');
-
-  button.addEventListener('click', function() {
-    window.alert('Colin Brennan: ' + text.value);
-  });
-
-  button.addEventListener('click', function() {
-    showresults.innerHTML = text.value;
-    });
 }
-
 window.addEventListener('load', init);
